e601e3c99b0514849dc7fae73b0a10767864c152
Sadman2539 <cadetsadman39@gmail.com>
2022-02-07 00:38:01 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>CHAGOL</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	</head>
+	<body>
+	<div class="wrapper indexPage">
+		<div class="mainSection">
+			<div class="logoContainer" >
+				<img src="assets/images/cagoal_logo.png">
+			</div>
+			<div class="searchContainer">
+				<form action="search.php" method="GET">
+					<input class="searchBox" type="text" name="term">
+					<input class="searchButton" type="submit" value="Search" >
+				</form>
+
+			</div>
+		</div>
+	</div>
+	</body>
+	</html>	
---
+++ search.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>CHAGOL</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	</head>
+	<body>
+	<div class="wrapper">
+		<div class="header">
+			<div class="headerContent">
+				<div class="logoContainer" >
+					<a href="index.php">
+					<img src="assets/images/cagoal_logo.png">
+					</a>
+				</div>
+				<div class="searchContainer">
+					
+					<form action="search.php" method="GET">
+						<div class="searchBoxContainer">
+							<input class="searchBox" type="text" name="term">
+							<button class="searchButton">
+								<img src="assets/images/icons/search_icon.png">
+							</button>
+						</div>
+					</form>
+						
+					
+				</div>
+			</div>	
+		</div>
+	</div>
+	</body>
+	</html>	
